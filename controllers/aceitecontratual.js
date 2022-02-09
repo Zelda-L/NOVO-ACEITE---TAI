f99a7d813b163714dc1dd39b151e531436858011
Tai Leal <tairini.leal@gmail.com>
2022-02-09 19:53:36 -0300

    test

--- controllers/aceitecontratual.js
+++ controllers/aceitecontratual.js
@@ -1,4 +1,4 @@
 module.exports = app => { 
 
-    app.get('/aceitecontratual', (req, res) => res.send('Você está na rota de aceite contratual, e está realizando um GET'))
+    app.get('/aceitecontratual', (req, res) => res.send('Você está na rota de aceite contratual, test e está realizando um GET'))
 }
